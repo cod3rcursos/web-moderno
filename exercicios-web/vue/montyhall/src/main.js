bfbd9f8324063f63f962fb4d1a90cac41d2bf4ac
Leonardo Leitão <leonardomleitao@gmail.com>
2018-06-15 10:18:07 -0300

    Correcao projetos vue

--- exercicios-web/vue/montyhall/src/main.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import App from './App'
-
-new Vue({
-    render: h => h(App)
-    // render(createElement) {
-    //     return createElement(App)
-    // }
-}).$mount("#app")
